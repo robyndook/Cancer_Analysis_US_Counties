713355c174a6b40f8d5d382fc578f56ae73509b4
robyndook <cook.robync@gmail.com>
2022-05-31 22:05:03 -0700

    Update DB CSV Files with Median Income Data

--- Database/Database_Queries_cook.sql
+++ Database/Database_Queries_cook.sql
@@ -85,6 +85,16 @@ CREATE TABLE Census_Population (
 	POPESTIMATE2019 INT NOT NULL,
 	PRIMARY KEY (County_State)
 );
+CREATE TABLE Median_Income (	
+	County_State VARCHAR NOT NULL,	
+	Abbrv VARCHAR NOT NULL,
+	Median_Household_Income_2015 INT NOT NULL,
+	Median_Household_Income_2016 INT NOT NULL,
+	Median_Household_Income_2017 INT NOT NULL,
+	Median_Household_Income_2018 INT NOT NULL,
+	Median_Household_Income_2019 INT NOT NULL,
+	PRIMARY KEY (County_State)
+);
 
 SELECT * FROM Age_Incidence;
 SELECT * FROM Race_Incidence;
@@ -94,6 +104,7 @@ SELECT * FROM Race_Mortality;
 SELECT * FROM Sex_Mortality;
 SELECT * FROM MUA_Index;
 SELECT * FROM Census_Population;
+SELECT * FROM Median_Income;
 
 
 DROP TABLE Age_Incidence CASCADE;
@@ -104,6 +115,8 @@ DROP TABLE Race_Mortality CASCADE;
 DROP TABLE Sex_Mortality CASCADE;
 DROP TABLE MUA_Index CASCADE;
 DROP TABLE Census_Population CASCADE;
+DROP TABLE Median_Income CASCADE;
+
 --Join Tables
 -- Age_Incidence
 Select ai.Average_Annual_Count,
@@ -121,13 +134,21 @@ Select ai.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Abbrv,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Age_Incidence_Cancer_DB
 FROM Age_Incidence as ai
 LEFT JOIN MUA_Index as mi
 ON ai.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON ai.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON ai.County_State = hi.County_State
 -- Race_Incidence
 Select ri.Average_Annual_Count,
 	ri.Primary_State_Name,
@@ -142,13 +163,21 @@ Select ri.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Abbrv,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Race_Incidence_Cancer_DB
 FROM Race_Incidence as ri
 LEFT JOIN MUA_Index as mi
 ON ri.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON ri.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON ri.County_State = hi.County_State
 -- Sex_Incidence
 Select si.Average_Annual_Count,
 	si.Primary_State_Name,
@@ -163,13 +192,21 @@ Select si.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Abbrv,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Sex_Incidence_Cancer_DB
 FROM Sex_Incidence as si
 LEFT JOIN MUA_Index as mi
 ON si.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON si.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON si.County_State = hi.County_State
 -- Age_Mortality
 Select am.Average_Annual_Count,
 	am.Primary_State_Name,
@@ -184,13 +221,21 @@ Select am.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Abbrv,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Age_Mortality_Cancer_DB
 FROM Age_Mortality as am
 LEFT JOIN MUA_Index as mi
 ON am.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON am.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON am.County_State = hi.County_State
 -- Race_Mortality
 Select rm.Average_Annual_Count,
 	rm.Recent_Trend,
@@ -207,13 +252,20 @@ Select rm.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Race_Mortality_Cancer_DB
 FROM Race_Mortality as rm
 LEFT JOIN MUA_Index as mi
 ON rm.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON rm.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON rm.County_State = hi.County_State
 -- Sex_Mortality
 Select sm.Average_Annual_Count,
 	sm.Primary_State_Name,
@@ -228,13 +280,20 @@ Select sm.Average_Annual_Count,
 	cp.POPESTIMATE2016,	
 	cp.POPESTIMATE2017,	
 	cp.POPESTIMATE2018,	
-	cp.POPESTIMATE2019
+	cp.POPESTIMATE2019,
+	hi.Median_Household_Income_2015,
+	hi.Median_Household_Income_2016,
+	hi.Median_Household_Income_2017,
+	hi.Median_Household_Income_2018,
+	hi.Median_Household_Income_2019
 INTO Sex_Mortality_Cancer_DB
 FROM Sex_Mortality as sm
 LEFT JOIN MUA_Index as mi
 ON sm.County_State = mi.County_State
 LEFT JOIN Census_Population as cp
 ON sm.County_State = cp.County_State
+LEFT JOIN Median_Income as hi
+ON sm.County_State = hi.County_State
 
 SELECT * FROM Age_Incidence_Cancer_DB;
 SELECT * FROM Race_Incidence_Cancer_DB;
@@ -242,4 +301,11 @@ SELECT * FROM Sex_Incidence_Cancer_DB;
 SELECT * FROM Age_Mortality_Cancer_DB;
 SELECT * FROM Race_Mortality_Cancer_DB;
 SELECT * FROM Sex_Mortality_Cancer_DB;
+
+DROP TABLE Age_Incidence_Cancer_DB CASCADE;
+DROP TABLE Race_Incidence_Cancer_DB CASCADE;
+DROP TABLE Sex_Incidence_Cancer_DB CASCADE;
+DROP TABLE Age_Mortality_Cancer_DB CASCADE;
+DROP TABLE Race_Mortality_Cancer_DB CASCADE;
+DROP TABLE Sex_Mortality_Cancer_DB CASCADE;
 
